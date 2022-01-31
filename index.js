894ceca277c1056615c0e2aae7e780c55ecc90b3
Moshe <moisherosenzweig@gmail.com>
2022-01-30 21:41:18 -0500

    Complete

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance){
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  if (distance <= 400) {
+    return "This one is on me!";
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (distance>2500) {
+    return "No can do.";
+  }
+  
+  if (distance>2000) {
+    return "I will gladly take your thirty bucks."
+  }
+  }
+
+
+function ternaryCheckCity(city){
+  
+ 
+    return city==="NYC" ?'Ok, sounds good.' : "No go.";
+  ;
+  }
+
+
+function switchOnCharmFromTip(tipAmount){
+  switch (tipAmount) {
+    case 'generous' : return 'Thank you so much.';
+    break;
+    case 'not as generous' : return 'Thank you.';
+  break;
+default: return 'Bye.';
+break;  }
 }
